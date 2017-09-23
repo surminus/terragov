7a5dda64079754ff22014e0e500b3b3e51095a9d
Laura Martin <surminus@gmail.com>
2017-09-23 11:41:19 +0100

    Add empty tests for other classes
    I will add these tests later.

---
+++ spec/cleaner_spec.rb
@@ -0,0 +1,6 @@
+require "spec_helper"
+
+describe Terragov::Cleaner do
+  describe "delete" do
+  end
+end
---
+++ spec/terraform_spec.rb
@@ -0,0 +1,6 @@
+require "spec_helper"
+
+describe Terragov::Terraform do
+  describe "execute" do
+  end
+end
