160dc89734d284fbd2e9f9ca034c6678c3fe39bd
Laura Martin <surminus@gmail.com>
2018-01-01 14:08:31 +0000

    Rubocop fixup for Git class
    There were only 3 offences detected.

--- lib/terragov/git.rb
+++ lib/terragov/git.rb
@@ -1,11 +1,17 @@
+# frozen_string_literal: true
+
 require 'git'
 require 'highline'
 
 module Terragov
+  # The Git class allows you force interactive output based upon
+  # the current branches in the repo and data directories.
   class Git
     def branch_name(directory)
       unless File.exist?(File.join(directory, '.git'))
-        exit unless HighLine.agree("#{directory} not a git repository, do you wish to continue?")
+        exit unless HighLine.agree(
+          "#{directory} not a git repository, do you wish to continue?"
+        )
       end
 
       branch = ::Git.open(directory).current_branch
