a10a7b8453ce1a272bc3fe8807f1edfa459f41b1
Laura Martin <surminus@gmail.com>
2018-01-01 16:03:12 +0000

    Rubocop attempt on BuildPaths
    This still errors unfortunately complaining that my methods are too long
    and complicated. I haven't been able to think of a good way to refactor
    them at present.

--- lib/terragov/buildpaths.rb
+++ lib/terragov/buildpaths.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 require_relative 'cli'
 
 module Terragov
+  # this class constructs a set of paths based upon
+  # our agreed project structure
   class BuildPaths
     def base(options = {})
       environment = options['environment']
@@ -8,23 +12,25 @@ module Terragov
       repo_dir = options['repo_dir']
       stack = options['stack']
       project = options['project']
-      extra   = options['extra']
 
       # Construct variables
-      terraform_dir              = File.join(repo_dir, 'terraform')
-      project_dir                = File.join(terraform_dir, "projects/#{project}")
-      backend_file               = File.join(project_dir, "#{environment}.#{stack}.backend")
-      common_data_dir            = File.join(data_dir, "common/#{environment}")
-      common_data                = File.join(common_data_dir, 'common.tfvars')
-      stack_common_data          = File.join(common_data_dir, "#{stack}.tfvars")
-      project_data_dir           = File.join(data_dir, "#{project}/#{environment}")
-      common_project_data        = File.join(project_data_dir, 'common.tfvars')
-      secret_common_project_data = File.join(project_data_dir, 'common.secret.tfvars')
-      stack_project_data         = File.join(project_data_dir, "#{stack}.tfvars")
-      secret_project_data        = File.join(project_data_dir, "#{stack}.secret.tfvars")
+      terraform_dir = File.join(repo_dir, 'terraform')
+      project_dir = File.join(terraform_dir, "projects/#{project}")
+      backend_file = File.join(project_dir, "#{environment}.#{stack}.backend")
+      common_data_dir = File.join(data_dir, "common/#{environment}")
+      common_data = File.join(common_data_dir, 'common.tfvars')
+      stack_common_data = File.join(common_data_dir, "#{stack}.tfvars")
+      project_data_dir = File.join(data_dir, "#{project}/#{environment}")
+      common_project_data = File.join(project_data_dir, 'common.tfvars')
+      secret_common_project_data = File.join(
+        project_data_dir, 'common.secret.tfvars'
+      )
+      stack_project_data = File.join(project_data_dir, "#{stack}.tfvars")
+      secret_project_data = File.join(
+        project_data_dir, "#{stack}.secret.tfvars"
+      )
 
-      # Return hash to enable testing
-      data_paths = {
+      {
         terraform_dir: terraform_dir,
         project_dir: project_dir,
         backend_file: backend_file,
@@ -40,18 +46,14 @@ module Terragov
     end
 
     def data_validation(path)
-      if File.exist?(path)
-        true
-      else
-        false
-      end
+      file.exist?(path)
     end
 
     def data_paths(options = {})
       # The path order is important for passing the var files in the correct
       # order to Terraform as that creates the hierarchy for overrides
       base = self.base(options)
-      paths = [
+      [
         base[:common_data],
         base[:stack_common_data],
         base[:common_project_data],
@@ -62,46 +64,55 @@ module Terragov
     end
 
     def check_var_files(paths = [])
-      $path_array = []
-      paths.each do |path|
-        $path_array << data_validation(path)
+      path_array = []
+      paths.each { |p| path_array << data_validation(p) }
+
+      return true if path_array.include?(true)
+
+      puts 'files checked: '
+      paths.each { |p| puts p }
+      false
+    end
+
+    def check_paths(paths)
+      [paths[:terraform_dir], paths[:project_dir]].each do |path|
+        abort("error: cannot find #{path}") unless data_validation(path)
       end
 
-      if $path_array.include? true
-        return true
-      else
-        puts 'Files checked: '
-        paths.each do |path|
-          puts path
-        end
-        return false
-      end
+      # specific error for backend to remove ambiguity
+      backend_err = [
+        "error: cannot find backend file (#{paths[:backend_file]}).",
+        'did you specify the right stack?'
+      ].join("\n")
+
+      abort(backend_err) unless data_validation(paths[:backend_file])
     end
 
     def build_command(options = {})
-      paths = base(options)
-      abort("Error: cannot find main repository (#{paths[:terraform_dir]})") unless data_validation(paths[:terraform_dir])
-      abort("Error: cannot find project (#{paths[:project_dir]}).") unless data_validation(paths[:project_dir])
-      abort("Error: cannot find backend file (#{paths[:backend_file]}).\nDid you specify the right stack?") unless data_validation(paths[:backend_file])
-      var_paths = data_paths(options)
-      abort("Error: cannot find any var files") unless check_var_files(var_paths)
+      check_paths(base(options))
 
-      $full_vars = []
+      var_paths = data_paths(options)
+      unless check_var_files(var_paths)
+        abort('Error: cannot find any var files')
+      end
+
+      full_vars = []
       data_paths(options).each do |path|
-        if data_validation(path)
-          # TODO: write sops class
-          if path == paths[:secret_project_data] || path == paths[:secret_common_project_data]
-            $full_vars << "-var-file <(sops -d #{path})"
-          else
-            $full_vars << "-var-file #{path}"
-          end
+        next unless data_validation(path)
+        case path
+        when paths[:secret_project_data], paths[:secret_common_project_data]
+          full_vars << "-var-file <(sops -d #{path})"
+        else
+          full_vars << "-var-file #{path}"
         end
       end
-      # If defining additional Terraform commands, they need to be passed in as one string,
-      # with any hyphens escaped twice so it does not conflict with commander CLI options
+
+      # if defining additional terraform commands, they need to be passed in as
+      # one string, with any hyphens escaped twice so it does not conflict
+      # with commander CLI options
       extra = options['extra'].to_s
-      $full_vars << extra.delete('\\')
-      $full_vars.join(' ')
+      full_vars << extra.delete('\\')
+      full_vars.join(' ')
     end
   end
 end
