bd99d489932415693da323f1e339ae8e97adcd0c
Laura Martin <surminus@gmail.com>
2018-02-07 17:10:25 +0100

    Add further end-to-end tests
    For apply, destroy and deploy.

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -5,6 +5,8 @@ require 'spec_helper'
 
 describe Terragov::Cli do
   describe 'run' do
+    # Put checks for ENV VAR and config files in the plan, but this mostly
+    # stays the same for applies and destroys so do not need to repeat
     describe 'plan' do
       default_command = %w(
         bin/terragov
@@ -72,6 +74,128 @@ describe Terragov::Cli do
 
         expected_output = default_init_output.join(" ") + default_plan_output.join(" ")
         command = default_command.join(" ") + " " + command_options.join(" ")
+        expect {
+          system(command)
+        }.to output(expected_output).to_stdout_from_any_process
+
+        ENV['TERRAGOV_CONFIG_FILE'] = nil
+      end
+    end
+    describe 'apply' do
+      default_command = %w(
+        bin/terragov
+        --dry-run
+        --skip-git-check
+        --force
+        apply
+      )
+
+      default_init_output = [
+        'terraform init -backend-config',
+        "#{File.join(Dir.pwd, 'spec/stub/terraform/projects/myproject/dev.mystack.backend')}\n"
+      ]
+      default_plan_output = [
+        "bash -c 'terraform apply -auto-approve",
+        "-var-file #{File.join(Dir.pwd, 'spec/stub/data/common/dev/common.tfvars')}",
+        "-var-file #{File.join(Dir.pwd, 'spec/stub/data/common/dev/mystack.tfvars')}",
+        "-var-file #{File.join(Dir.pwd, 'spec/stub/data/myproject/dev/common.tfvars')}",
+        "-var-file <(sops -d #{File.join(Dir.pwd, 'spec/stub/data/myproject/dev/common.secret.tfvars')})",
+        "-var-file #{File.join(Dir.pwd, 'spec/stub/data/myproject/dev/mystack.tfvars')}",
+        "-var-file <(sops -d #{File.join(Dir.pwd, 'spec/stub/data/myproject/dev/mystack.secret.tfvars')}) '\n"
+      ]
+
+      it 'everything specified from CLI options' do
+        command_options = [
+          '-d spec/stub/data',
+          '-r spec/stub',
+          '-p myproject',
+          '-s mystack',
+          '-e dev',
+        ]
+
+        expected_output = default_init_output.join(" ") + default_plan_output.join(" ")
+        command = default_command.join(" ") + " " + command_options.join(" ")
+        expect {
+          system(command)
+        }.to output(expected_output).to_stdout_from_any_process
+      end
+    end
+    describe 'destroy' do
+      default_command = %w(
+        bin/terragov
+        --dry-run
+        --skip-git-check
+        --force
+        destroy
+      )
+
+      default_init_output = [
+        'terraform init -backend-config',
+        "#{File.join(Dir.pwd, 'spec/stub/terraform/projects/myproject/dev.mystack.backend')}\n"
+      ]
+      default_plan_output = [
+        "bash -c 'terraform destroy -force",
+        "-var-file #{File.join(Dir.pwd, 'spec/stub/data/common/dev/common.tfvars')}",
+        "-var-file #{File.join(Dir.pwd, 'spec/stub/data/common/dev/mystack.tfvars')}",
+        "-var-file #{File.join(Dir.pwd, 'spec/stub/data/myproject/dev/common.tfvars')}",
+        "-var-file <(sops -d #{File.join(Dir.pwd, 'spec/stub/data/myproject/dev/common.secret.tfvars')})",
+        "-var-file #{File.join(Dir.pwd, 'spec/stub/data/myproject/dev/mystack.tfvars')}",
+        "-var-file <(sops -d #{File.join(Dir.pwd, 'spec/stub/data/myproject/dev/mystack.secret.tfvars')}) '\n"
+      ]
+
+      it 'everything specified from CLI options' do
+        command_options = [
+          '-d spec/stub/data',
+          '-r spec/stub',
+          '-p myproject',
+          '-s mystack',
+          '-e dev',
+        ]
+
+        expected_output = default_init_output.join(" ") + default_plan_output.join(" ")
+        command = default_command.join(" ") + " " + command_options.join(" ")
+        expect {
+          system(command)
+        }.to output(expected_output).to_stdout_from_any_process
+      end
+    end
+    describe 'deploy' do
+      default_command = %w(
+        bin/terragov
+        --dry-run
+        --skip-git-check
+        --force
+        deploy
+      )
+
+      default_deploy_output = []
+
+      %w(myproject second-project third-project).each do |project|
+        default_deploy_output << [
+          'terraform init -backend-config',
+          "#{File.join(Dir.pwd, "spec/stub/terraform/projects/#{project}/dev.mystack.backend")}\n"
+        ].join(" ")
+        default_deploy_output << [
+          "bash -c 'terraform plan -detailed-exitcode",
+          "-var-file #{File.join(Dir.pwd, 'spec/stub/data/common/dev/common.tfvars')}",
+          "-var-file #{File.join(Dir.pwd, 'spec/stub/data/common/dev/mystack.tfvars')}",
+          "-var-file #{File.join(Dir.pwd, "spec/stub/data/#{project}/dev/common.tfvars")}",
+          "-var-file <(sops -d #{File.join(Dir.pwd, "spec/stub/data/#{project}/dev/common.secret.tfvars")})",
+          "-var-file #{File.join(Dir.pwd, "spec/stub/data/#{project}/dev/mystack.tfvars")}",
+          "-var-file <(sops -d #{File.join(Dir.pwd, "spec/stub/data/#{project}/dev/mystack.secret.tfvars")}) '\n"
+        ].join(" ")
+      end
+
+      it 'everything specified froconfig file' do
+        command_options = [
+          '--file spec/stub/deployment.yaml',
+          '--command plan',
+        ]
+
+        ENV['TERRAGOV_CONFIG_FILE'] = 'spec/stub/myconfig.yml'
+        expected_output = default_deploy_output.join("")
+        command = default_command.join(" ") + " " + command_options.join(" ")
+
         expect {
           system(command)
         }.to output(expected_output).to_stdout_from_any_process
